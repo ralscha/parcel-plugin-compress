71802c0a3f15f89f371cfe7d81503349c4aaefaf
Ralph Schaer <ralphschaer@gmail.com>
2020-04-23 19:53:09 +0200

    Resolves #9

--- brotliAdapter.js
+++ brotliAdapter.js
@@ -1,11 +1,8 @@
-const chalk = require('chalk');
-
 const brotliAdapter = () => {
 	const zlib = require('zlib');
 	if (zlib.brotliCompressSync) {
 		return { isZlib: true, compress: zlib.brotliCompressSync };
 	} else {
-		console.log(chalk.yellow('Compression warning: zlib too old. Falling back to brotli.'));
 		const brotli = require('brotli');
 		return brotli;
 	}
--- zopfliAdapter.js
+++ zopfliAdapter.js
@@ -1,12 +1,8 @@
-const chalk = require('chalk');
-
 const zopfliAdapter = () => {
 	try {
 		const zopfli = require('node-zopfli-es');
 		return zopfli;
 	} catch (err) {
-		console.log(chalk.yellow('Compression warning: node-zopfli-es could not be loaded. Falling back to @gfx/zopfli.'))
-
 		const zopfli = require('@gfx/zopfli');
 		return zopfli;
 	}
